87fd233ca78f77b52014fa4c1af8f1850b930648
antoine <antoine.patron.62610@gmail.com>
2026-06-14 23:52:30 +0200

    Revert "Sweep closed/stale docs previews"
    This reverts commit 4634b698f944c1e9a1f571b4b0ad8126a71294a2.

---
+++ site/docs-shared/pr-2/doxygen/namespaceliara_1_1preview.js
@@ -0,0 +1,12 @@
+var namespaceliara_1_1preview =
+[
+    [ "RingBuffer", "classliara_1_1preview_1_1RingBuffer.html", "classliara_1_1preview_1_1RingBuffer" ],
+    [ "Severity", "namespaceliara_1_1preview.html#afa51de691e84a6afa6b94371e8995e47", [
+      [ "Trace", "namespaceliara_1_1preview.html#afa51de691e84a6afa6b94371e8995e47add4ec0ac4e58f7c32a01244ae91150b1", null ],
+      [ "Info", "namespaceliara_1_1preview.html#afa51de691e84a6afa6b94371e8995e47a4059b0251f66a18cb56f544728796875", null ],
+      [ "Warning", "namespaceliara_1_1preview.html#afa51de691e84a6afa6b94371e8995e47a0eaadb4fcb48a0a0ed7bc9868be9fbaa", null ],
+      [ "Fatal", "namespaceliara_1_1preview.html#afa51de691e84a6afa6b94371e8995e47a882384ec38ce8d9582b57e70861730e4", null ]
+    ] ],
+    [ "sum", "namespaceliara_1_1preview.html#ab3aee8468906a7db6021f250655f8f0d", null ],
+    [ "to_string", "namespaceliara_1_1preview.html#a96a382d569944fb05a1f2835df2ad0bd", null ]
+];
---
+++ site/docs-shared/pr-2/doxygen/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['head_5f_0',['head_',['../classliara_1_1preview_1_1RingBuffer.html#a9adf5003a9fc27a1618bf687424cedde',1,'liara::preview::RingBuffer']]]
+];
